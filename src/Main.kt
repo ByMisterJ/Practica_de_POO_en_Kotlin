04aad123af748bf86ba4d1e71d5e875b8cfe06b2
josarapar <josearanda9000@gmail.com>
2025-10-03 20:17:09 +0200

    Ajuste y reubicacion de funciones a Tarea.kt desde Main.kt, marcarComoCompletada y mostrarInfo

--- src/Main.kt
+++ src/Main.kt
@@ -2,17 +2,3 @@ fun main() {
     println("Hello, World!")
 
 }
-fun marcarComoCompletada(tarea: Tarea) {
-    tarea.completada = true
-    println("Tarea completada.")
-}
-
-fun mostrarInfo(tarea: Tarea) {
-    if (tarea.completada)
-        println("[:)]")
-    else
-        println("[:(]")
-    print(" " + tarea.id)
-    print(" " + tarea.titulo)
-    print(" " + tarea.descripcion)
-}
--- src/Tarea.kt
+++ src/Tarea.kt
@@ -4,3 +4,18 @@ class Tarea {
     var descripcion: String? = null
     var completada: Boolean = false
 }
+
+fun marcarComoCompletada(tarea: Tarea) {
+    tarea.completada = true
+    println("Tarea completada.")
+}
+
+fun mostrarInfo(tarea: Tarea) {
+    if (tarea.completada)
+        println("[:)]")
+    else
+        println("[:(]")
+    print(" " + tarea.id)
+    print(" " + tarea.titulo)
+    print(" " + tarea.descripcion)
+}
